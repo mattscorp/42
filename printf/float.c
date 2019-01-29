b4813f463e893027968fdd9dfaa4dd172f5fac9e
matt <mac_under@unders-MacBook-Pro.local>
2019-01-29 12:07:14 +0100

    float ok sauf pour approx en dessous de 5

--- printf/float.c
+++ printf/float.c
@@ -12,46 +12,85 @@
 
 #include "ft_printf.h"
 
-static char	*ft_approx(char *rtn)
+static char	*ft_approx(char *deci)
 {
 	int	i;
 
-	i = ft_strlen(rtn);
+	i = ft_strlen(deci);
 	i--;
-		if (rtn[i] >= 5)
+	printf("i = %d\n", i);
+	 while(deci[i] == '9' )
+	{
+		deci[i] = '0';
+		i--;
+	
+		if (deci[i] != '9')
 		{
-			rtn[i] = '0';
-			rtn[i - 1] = rtn[i - 1] + 1;
+			deci[i] = deci[i] + 1;
+			break;
 		}
-	return (rtn);
+	}
+/*	i = ft_strlen(deci);
+	i--;
+	printf("iii = %d\n", i);
+	printf("idecii = %c\n", deci[i]);
+		if (deci[i] < '5')
+		{
+			deci[i] = deci[i] + 1;
+		}	
+//	}
+//	while (deci[i])
+//	{
+//		if (!(deci[i] >= '0' && deci [i] <= '9'))
+//			deci[i] = '0';
+//		i++;
+//	}
+*/	return (deci);
 }
 
 char    *ft_float(va_list ap)
 {
-    float   f;
+	float   f;
 	long	tmp;
 	char	*rtn;
 	char	*deci;
-	int		len;
+	int	len;
 
-    f = va_arg(ap, double);
+	f = va_arg(ap, double);
 	tmp = f;
 	len = ft_strlen(ft_itoalong(tmp));
 	if (!(rtn = (char*)malloc(sizeof(char) * (len + 7))))
 		return (NULL);
 	rtn = ft_itoalong(tmp);
 	rtn[len] = '.';
+	printf("f avant =%f\n", f);
 	f = f - tmp;
+	printf("f - tmp =%f\n", f);
 	f = f * 1000000;
+	printf("f * 1000000 =%f\n", f);
 	if (!(deci = (char*)malloc(sizeof(char) * 7)))
 		return (NULL);
-    	tmp = f;
-		deci = ft_itoalong(tmp);
-		if (f < 0)
-			rtn = ft_strjoin(rtn, deci + 1);
-		else	
-			rtn = ft_strjoin(rtn, deci);
-	rtn = ft_approx(rtn);
-		printf("rtn FINAL = %s\n", rtn);
+	deci[7] = '\0';
+	tmp = f;
+	printf("tmp =%ld\n", tmp);
+
+	deci = ft_itoalong(tmp);
+	printf("deci  =%s\n", deci);
+	deci = ft_approx(deci);
+	printf("deci approx =%s\n", deci);
+
+	if (f < 0)
+		rtn = ft_strjoin(rtn, deci + 1);
+	else	
+		rtn = ft_strjoin(rtn, deci);
+/*	len = 0;
+	while(rtn[len])
+	{
+		if (!(rtn[len] >= '0' && rtn[len] <= '9') && !(rtn[len] == '.' ))
+			rtn[len] = '0';
+		len++;
+		printf("len = %d\n", len);
+	}
+*/	printf("rtn FINAL = %s\n", rtn);
 	return (rtn);
 }
